1b6128ce0dedec47e34b33160824dc5f8cf3e562
sydneyjenk <57198924+sydneyjenk@users.noreply.github.com>
2019-11-07 16:15:43 -0500

    Update Channel.java
    added encrypt and decrypt :)

--- Channel.java
+++ Channel.java
@@ -1,9 +1,24 @@
 import java.util.*;
 
 public class Channel 
-{
+{ 
+	int p = a; 
+	int q = b ; 
+	int n = p * q ; 
+	int e = 11 ; 
 	
 	
 	
+	
+	public encrypt(String x) {
+		
+		
+	
+	
+	}
+	public decrypt{
+		
+		
+	}
 	
 }	//	End Channel class
