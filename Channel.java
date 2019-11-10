5ecf5c25ebabb7903789106fe26f5a7f5b902e69
dport17 <57191783+dport17@users.noreply.github.com>
2019-11-10 15:51:06 -0500

    Added untested encrypt, decrypt.

--- Channel.java
+++ Channel.java
@@ -2,11 +2,17 @@ import java.util.*;
 
 public class Channel 
 { 
+	//the two prime numbers from which the keys will be fashioned
 	BigNumber p; 
 	BigNumber q; 
+	//for n in both keys
 	BigNumber n;
+	//for the value of n
 	BigNumber pTimesq=new BigNumber("");
-	int e = 11 ; 
+	//for the public key
+	BigNumber e;
+	//for the private key
+	BigNumber d;
 	public Channel(BigNumber p, BigNumber q) {
 		if(p.isPrime()&&q.isPrime()) {
 			this.p=p;this.q=q;
@@ -18,16 +24,18 @@ public class Channel
 	}//end Channel constructor
 	
 	
-	
-	
-	
-	public encrypt(String x) {
-		
+	public BigNumber encrypt(BigNumber m) {
+		BigNumber c;
+		c=(m.toThePowerOf(e));
+		c=BigNumber.modulus(c,n);
+		return c;
 	
 	}//end encrypt
-	public decrypt{
-		
-		
+	public BigNumber decrypt(BigNumber c){
+		BigNumber m;
+		m=(c.toThePowerOf(d));
+		m=BigNumber.modulus(m, n);
+		return m;
 	}//end decrypt
 	
 }	//	End Channel class
