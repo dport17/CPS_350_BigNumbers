a71bd74ed794215f307b88bd205caa1efc2d10ac
dport17 <57191783+dport17@users.noreply.github.com>
2019-11-10 15:16:14 -0500

    Started work on the channel constructor.
    Stopped since Sydney is taking the lead on that now :)

--- Channel.java
+++ Channel.java
@@ -2,22 +2,32 @@ import java.util.*;
 
 public class Channel 
 { 
-	int p = null; 
-	int q = null; 
-	int n = p * q; 
-	int e = 11; 
+	BigNumber p; 
+	BigNumber q; 
+	BigNumber n;
+	BigNumber pTimesq=new BigNumber("");
+	int e = 11 ; 
+	public Channel(BigNumber p, BigNumber q) {
+		if(p.isPrime()&&q.isPrime()) {
+			this.p=p;this.q=q;
+			n=pTimesq.multi(this.p,this.q);
+		}
+		else {
+			System.out.println("p, q, or both are not prime.");
+		}
+	}//end Channel constructor
 	
-	public encrypt(BigNumber m) 
-	{
+	
+	
+	
+	
+	public encrypt(String x) {
+		
+	
+	}//end encrypt
+	public decrypt{
 		
 		
+	}//end decrypt
 	
-	
-	}	//	End encrypt method
-	
-	public decrypt(BigNumber c)
-	{
-		
-		
-	}	//	End decrypt method
 }	//	End Channel class
