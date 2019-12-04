15964a6a0161bf28253bcd2c1ba061a6eec72eb2
wongb2 <57199009+wongb2@users.noreply.github.com>
2019-12-04 09:49:46 -0500

    Finished AVL Tree
    Still missing the copy constructor, but I don't know how to approach that.

--- AVLTree.java
+++ AVLTree.java
@@ -1,112 +1,276 @@
-import java.util.*;
-
 /*
  * Names: Paul Scheeler, Devin Porter, Sydney Jenkins, Brandon Wong
  * CPS 350 01
  * Project 3
  * 
+ * AVLTree.java
  */
 
 public class AVLTree extends TreeNode
 {
 	TreeNode root;
-	int balanceFactor;
-	int counter = 0;		// Initialized at 0 for populating the arrays in the traversal methods
 	
-	int[] order = new int[100];	// Initialized to handle 100 nodes
+	int counter = 0;		//	Initialized at 0 for populating the arrays in preOrder method
+	int counter2 = 0;		//	Initialized at 0 for populating the arrays in inOrder method
+	int counter3 = 0;		//	Initialized at 0 for populating the arrays in postOrder method
 	
 	public AVLTree()
 	{
-		root = null;			// Sets 'root' to null if not given a root value
-		balanceFactor = 0;		// Sets the default balance factor to 0 since there's only the root
+		root = null;		//	Sets 'root' to null if not given a root value
 	
 	} //	End default constructor
 	
 	public AVLTree(int v)
 	{
-		root = new TreeNode(v);		// Creates new root node with the value 'v'
-		balanceFactor = 0;		// Sets the default balance factor to 0 since there's only the root
+		root = new TreeNode(v);	//	Creates new root node with the value 'v'
 	
 	} //	End partial constructor
 	
-	public AVLTree(int v, TreeNode child)
+	public int height(TreeNode r)
 	{
-		root = new TreeNode(v, child);	// Creates new root node with the value 'v' and one reference to a child node
-		balanceFactor = 0;		// Sets the default balance factor to 0
+		if (r == null)			return 0;
+		else				return r.height;
 		
-		//	Since there will only be one parent node and one child node, there is no need for a loop
-		if (root.left != null)		balanceFactor--;	// If the child node is a left child,	balanceFactor = -1
-		if (root.right != null)	 	balanceFactor++;	// If the child node is a right child,	balanceFactor = +1
-		
-	} //	End partial constructor
+	} //	End height method
 	
-	public AVLTree(int v, TreeNode child1, TreeNode child2)
+	public int max(int a, int b)
 	{
-		root = new TreeNode(v, child1, child2);		// Creates new root node with the value 'v' and two references to children nodes
-		balanceFactor = 0;				// Sets the default balance factor to 0 since it is balanced upon initialization
+		if (a > b)		return a;
+		else			return b;
 		
-	} //	End full constructor
+	} //	End max method
 	
-	public int[] preOrder (TreeNode r)
+    public int getBalanceFactor(TreeNode r) 
+    { 
+        if (r == null) 			return 0; 
+        else			 	return height(r.left) - height(r.right); 
+        
+    } // 	End getBalanceFactor method
+	
+    public TreeNode rightRotate(TreeNode r) 
+    {
+    	//	Create new nodes that will be moved during the rotation
+        TreeNode temp = r.left; 
+        TreeNode temp2 = temp.right; 
+  
+        // 	Perform the rotation 
+        temp.right = r; 
+        r.left = temp2; 
+  
+        // 	Update the height of each path 
+        r.height = 	max(height(r.left), height(r.right)) + 1; 
+        temp.height = 	max(height(temp.left), height(temp.right)) + 1; 
+  
+        // 	Return the new root of the rotated section
+        return temp; 
+        
+    } //	End rightRotate method
+    
+    public TreeNode leftRotate(TreeNode r) 
+    {
+    	//	Create new nodes that will be moved during the rotation
+        TreeNode temp = r.right; 
+        TreeNode temp2 = temp.left; 
+  
+        // 	Perform the rotation 
+        temp.left = r; 
+        r.right = temp2; 
+  
+        //  	Update the height of each path 
+        r.height = max(height(r.left), height(r.right)) + 1; 
+        temp.height = max(height(temp.left), height(temp.right)) + 1; 
+  
+        // 	Return the new root of the rotated section
+        return temp; 
+        
+    } //	End leftRotate method
+	
+	public TreeNode insert(TreeNode r, int insertValue)
+	{			
+		//	Create the new TreeNode
+		if (r == null)		return (new TreeNode(insertValue));
+		
+		if 	(insertValue < r.value)		r.left = insert(r.left, insertValue);
+		else if (insertValue > r.value)		r.right = insert(r.right, insertValue);
+		else					return r; // 	Do nothing since there duplicates are not allowed.
+		
+		//	This updates the height of a path
+		r.height = 1 + max(height(r.left), height(r.right));	
+		
+		//	This gets the balanceFactor to see if it's less than -1 or greater than 1 (unbalanced)
+		int balanceFactor = getBalanceFactor(r); 		
+		
+        if (balanceFactor > 1 && insertValue < r.left.value) 		return rightRotate(r); 	// 	Right rotation
+        if (balanceFactor < -1 && insertValue > r.right.value) 		return leftRotate(r);	//	Left rotation
+  
+        // 	Left-Right rotation 
+        if (balanceFactor > 1 && insertValue > r.left.value) 
+        { 
+            r.left = leftRotate(r.left);	// 	Left Rotation first
+            return rightRotate(r); 		//	Right Rotation second
+            
+        } 
+  
+        // 	Right-Left rotation
+        if (balanceFactor < -1 && insertValue < r.right.value) 
+        { 
+            r.right = rightRotate(r.right); 	//	Right Rotation first
+            return leftRotate(r); 		//	Left Rotation second
+            
+        } 
+  
+        return r; 
+        
+	} //	End insert method
+	
+    //	The node 'r' in the parameters is the right subtree of the deleted node
+    public TreeNode minValueNode(TreeNode r)  
+    {  
+        TreeNode iter = r;  
+  
+        //	This while loop traverses the left tree of the passed right subtree (r)
+        while (iter.left != null)		iter = iter.left;  
+        
+        // 	This node is the minimum value node in the deleted node's right subtree
+        return iter;  
+        
+    } //	End minValueNode method
+    
+    TreeNode delete(TreeNode r, int deleteValue)  
+    {   
+        if (r == null)  		return r;  
+  
+        // 	If the 'deleteValue' is less than 'r.value', it is in the left subtree 
+        if (deleteValue < r.value)  		r.left = delete(r.left, deleteValue);  
+  
+        // 	If the 'deleteValue' is greater than 'r.value', it is in the right subtree 
+        else if (deleteValue > r.value)  	r.right = delete(r.right, deleteValue);  
+  
+        // 	If the 'deleteValue' is equal to 'r.value', this is the node to be deleted
+        else
+        {  
+            //	Executes if the node has only one child or no child  
+            if ((r.left == null) || (r.right == null))  
+            {  
+                TreeNode temp = null;  
+                
+                if (temp == r.left)  	temp = r.right;  
+                else					temp = r.left;  
+  
+                // 	Executes if there is no child node (temp stays null after the reassignment)
+                if (temp == null)  
+                {  
+                    temp = r;  
+                    r = null;  
+                }  
+                
+                // 	Executes if there is one child node
+                else   					r = temp; 	// Copy the non-empty child  
+                
+            }  
+            
+            //	Executes if the node has two children
+            else
+            {  
+                // 	Get the smallest value in the right subtree 
+                TreeNode temp = minValueNode(r.right);  
+  
+                // 	Copy the smallest value in the right subtree
+                r.value = temp.value;  
+  
+                // 	Delete the original child node
+                r.right = delete(r.right, temp.value);  
+            }  
+        }  
+  
+        // 	If the tree had only one node then return  
+        if (r == null)  			return r;  
+  
+	//	This updates the height of a path
+        r.height = 1 + max(height(r.left), height(r.right));  
+  
+	//	This gets the balanceFactor to see if it's less than -1 or greater than 1 (unbalanced)
+        int balanceFactor = getBalanceFactor(r);  
+        
+        if (balanceFactor > 1 && getBalanceFactor(r.left) >= 0)  	return rightRotate(root);	// Right rotation
+        if (balanceFactor < -1 && getBalanceFactor(r.right) <= 0)  	return leftRotate(root);	// Left rotation
+        
+        // 	Left-Right rotation  
+        if (balanceFactor > 1 && getBalanceFactor(r.left) < 0)  
+        {  
+            r.left = leftRotate(r.left);  
+            return rightRotate(r);  
+        }  
+        
+        // 	Right-Left rotation  
+        if (balanceFactor < -1 && getBalanceFactor(r.right) > 0)  
+        {  
+            r.right = rightRotate(r.right);  
+            return leftRotate(r);  
+        }  
+  
+        return r;  
+        
+    } //	End delete method
+    
+	public int[] preOrder (TreeNode r, int[] a)
 	{
 		if (r != null)
 		{
-			order[counter] = r.value;
-			counter++;	// Increment the counter
+			a[counter] = r.value;
+			counter++;		//	Increment the counter
 
-			preOrder(r.left);
-			preOrder(r.right);
+			preOrder(r.left, a);
+			preOrder(r.right, a);
 			
-		} //	End if statement
+		}	//	End if statement
 		
 		//	I don't believe this will ever execute?
-		else	return order;
+		else	return a;
 		
 		//	Returns the array
-		return order;
+		return a;
 		
 	} //	End preOrder method
 	
-	public int[] inOrder (TreeNode r)
+	public int[] inOrder (TreeNode r, int[] a)
 	{
 		if (r != null)
 		{
-			order[counter] = r.value;
-			counter++;	// Increment the counter
-
-			inOrder(r.left);
-			inOrder(r.right);
+			inOrder(r.left, a);
 			
-		} //	End if statement
+			a[counter2] = r.value;
+			counter2++;		//	Increment the counter
+			
+			inOrder(r.right, a);
+			
+		}	//	End if statement
 		
 		//	I don't believe this will ever execute?
-		else	return order;
+		else	return a;
 		
 		//	Returns the array
-		return order;
+		return a;
 		
 	} //	End inOrder method
 	
-	public int[] postOrder (TreeNode r)
+	public int[] postOrder (TreeNode r, int[] a)
 	{
 		if (r != null)
 		{
-			order[counter] = r.value;
-			counter++;	// Increment the counter
-
-			postOrder(r.left);
-			postOrder(r.right);
+			postOrder(r.left, a);
+			postOrder(r.right, a);
 			
-		} //	End if statement
+			a[counter3] = r.value;
+			counter3++;		//	Increment the counter
+			
+		}	//	End if statement
 		
 		//	I don't believe this will ever execute?
-		else	return order;
+		else	return a;
 		
 		//	Returns the array
-		return order;
+		return a;
 		
-	} //	End postOrder method
-	
-	//	NOTE: Still have to implement copy, insertion, and deletion
-	
+	} //	End postOrder method	
 } //	End AVL Tree class
